47865e0e3595867e395ad9767293369cb78ee00b
Muffin bit <mix9muffin@gmail.com>
2021-08-16 14:51:58 -0700

    Updating data csv, updating data csv path, removing debugger statements

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@
 
 // Parse line of csv into an object
 function parseLine(row) {
-    debugger;
     var person = {};
     person.id = row["Contestant ID (internal)"];
     person.group = row["Group Association"]
@@ -15,7 +14,6 @@ function parseLine(row) {
 }
 
 function showProfiles(data) {
-    debugger;
     data.forEach(function(d) {
       // Make it show up on the page
       var markup = "<tr><td>" + d.nameEnglish + "</td><td>" + d.group + "</td><td><img <img style=\"display:block;\" width=\"100%\" height=\"100%\"src=" + d.profilePic1 + "></td></tr>";
@@ -26,7 +24,7 @@ function showProfiles(data) {
 
 function main() {
   // Get data
-  d3.csv("https://muffin-bit.github.io/gp999/data/muffin-bit.github.io_gp999 - Profile Info.csv", parseLine, function (err, data) {
+  d3.csv("https://muffin-bit.github.io/gp999/data.csv", parseLine, function (err, data) {
       showProfiles(data);
   });
 }
