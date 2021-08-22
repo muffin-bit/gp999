b6f2c128f75fdc140e05fd0f45e2cf725c4d2490
Pie Byte <mix9muffin@gmail.com>
2021-08-22 13:14:48 -0700

    make navbar highlight current page again

---
+++ components/navbar.js
@@ -0,0 +1,33 @@
+class Header extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
+      <div id="banner">
+        <img src="img/Girls_Planet_999_Logo_EN.png">
+        <div class="topnav">
+          <div class="leftnav">
+            <a href="index.html">Home</a>
+            <a href="contestants.html">Contestants</a>
+            <a href="performances.html">Performances</a>
+          </div>
+          <div class="rightnav">
+            <p>Made by Muffin and Pie</p>
+        </div>
+        </div>
+      </div>
+    `;
+  }
+}
+
+$(function(){
+  $('.leftnav a').each(function(){
+    if ($(this).prop('href') == window.location.href) {
+      $(this).addClass('current');
+    }
+  });
+});
+
+customElements.define('navbar-component', Header);
